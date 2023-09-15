9f28b8644b1157136e73ef8639a68221ce4a0fff
webtam81 <t.h@tammyharris.co.uk>
2023-09-15 19:18:36 +0100

    added bootstrap

---
+++ script.js
@@ -0,0 +1,15 @@
+//VARIABLES
+
+//FUNCTIONS
+
+//EVENT LISTENERS
+
+//TODO
+
+//DONE
+
+//NOTES
+//query url needs latitude and longitude. city name will be entered on the web page. use api to translate city to lat/lond and put in variables for this url
+//searched cities go in left column and creates a button
+//when these buttons are clicked the weather info goes in the right
+//store it all in local storage
