6d1b6788aa3710d3ff361884b9ee90d361ea19f2
webtam81 <t.h@tammyharris.co.uk>
2023-09-19 16:42:43 +0100

    added function to render location buttons

--- script.js
+++ script.js
@@ -4,10 +4,34 @@ let longitude;
 let queryURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=d1812ca69b57b9e8fd8ff23d673f0f07';
 let searchInput;
 let searchBtn = $('#search-button');
+let locations = ['Birmingham', 'Toronto', 'Aberdeen']; //TODO change to empty
+let searchHistoryEl = $('#history');
 
 //FUNCTIONS
 function addLocation () {
 
+}
+
+function renderLocations () {
+    console.log(locations);
+    if (locations.length < 1) {
+        return;
+    } else {
+        for (let i = 0; i < locations.length; i++) {
+            let newButton = $('<button>');
+            newButton.addClass('location');
+            newButton.text(locations[i]);
+            searchHistoryEl.append(newButton);
+        }
+    }
+}
+
+function saveLocations () {
+    //save locations to localstorage
+}
+
+function getLocations () {
+    //get locations from localstorage
 }
 
 
@@ -16,6 +40,8 @@ searchBtn.on('click', function() {
     event.preventDefault();
     searchInput = $('#search-input').val();
     console.log(`You searched ${searchInput}`);
+    //add this to local storage
+    //create button from this and add to aside
 });
 
 //TODO
@@ -27,3 +53,6 @@ searchBtn.on('click', function() {
 //searched cities go in left column and creates a button
 //when these buttons are clicked the weather info goes in the right
 //store it all in local storage
+
+//TESTS / TEMP
+renderLocations ();
