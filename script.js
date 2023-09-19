b40bce559ff3e2aaaaa02b893f61c1f3ebe6694b
webtam81 <t.h@tammyharris.co.uk>
2023-09-19 23:42:23 +0100

    stored searches in localstorage

--- script.js
+++ script.js
@@ -4,28 +4,35 @@ let longitude;
 let queryURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=d1812ca69b57b9e8fd8ff23d673f0f07';
 let searchInput;
 let searchBtn = $('#search-button');
-let locations = ['Birmingham', 'Toronto', 'Aberdeen']; //TODO change to empty
+//let locationsArray = ['Birmingham', 'Toronto', 'Aberdeen']; //TODO change to empty
+let locationsArray = [];
 let searchHistoryEl = $('#history');
 
 //FUNCTIONS
 //add location to side bar
 function addLocation () {
+    //add this to array
+    locationsArray.push(searchInput);
+    console.log(locationsArray);
+    //create button
     let newButton = $('<button>');
     newButton.addClass('location');
     newButton.text(searchInput);
     searchHistoryEl.append(newButton);
+    saveLocations ();
 }
 
 //render locations already in array
 function renderLocations () {
-    console.log(locations);
-    if (locations.length < 1) {
+    getLocations ();
+    console.log(locationsArray);
+    if (locationsArray.length < 1) {
         return;
     } else {
-        for (let i = 0; i < locations.length; i++) {
+        for (let i = 0; i < locationsArray.length; i++) {
             let newButton = $('<button>');
             newButton.addClass('location');
-            newButton.text(locations[i]);
+            newButton.text(locationsArray[i]);
             searchHistoryEl.append(newButton);
         }
     }
@@ -34,11 +41,15 @@ function renderLocations () {
 //save array to local storage
 function saveLocations () {
     //save locations to localstorage
+    localStorage.setItem('savedlocations',JSON.stringify(locationsArray));
 }
 
 //get array from local storage
 function getLocations () {
     //get locations from localstorage
+    if (localStorage.getItem('savedlocations')) {
+        locationsArray = JSON.parse(localStorage.getItem('savedlocations'));
+    }
 }
 
 
@@ -49,8 +60,7 @@ searchBtn.on('click', function() {
     event.preventDefault();
     searchInput = $('#search-input').val();
     console.log(`You searched ${searchInput}`); //TODO rm
-    //add this to array
-    locations.push(searchInput);
+    
     //create button from this and add to aside
     addLocation (); 
 });
