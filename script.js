3f65ef7cec083e9b1dcf1e551ad2fb00c2b5170c
webtam81 <t.h@tammyharris.co.uk>
2023-11-02 01:08:35 +0000

    reshuffling

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //VARIABLES
 let latitude;
 let longitude;
-let queryURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=d1812ca69b57b9e8fd8ff23d673f0f07';
 let searchInput;
+let geocodedLocation;
+
+let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=d1812ca69b57b9e8fd8ff23d673f0f07';
+let geocodingURL;
+
 let searchBtn = $('#search-button');
 //let locationsArray = ['Birmingham', 'Toronto', 'Aberdeen']; //TODO change to empty
 let locationsArray = [];
@@ -12,20 +16,24 @@ let searchHistoryEl = $('#history');
 //add location to side bar
 function addLocation () {
     //add this to array
-    locationsArray.push(searchInput);
-    console.log(locationsArray);
-    //create button
-    let newButton = $('<button>');
-    newButton.addClass('location');
-    newButton.text(searchInput);
-    searchHistoryEl.append(newButton);
-    saveLocations ();
+    if (searchInput == '') {
+        return;
+    } else {
+        locationsArray.push(searchInput);
+        //console.log(locationsArray); //TODO rm
+        //create button and add to side bar
+        let newButton = $('<button>');
+        newButton.addClass('location');
+        newButton.text(searchInput);
+        searchHistoryEl.append(newButton);
+        saveLocations ();
+    }
 }
 
 //render locations already in array
 function renderLocations () {
     getLocations ();
-    console.log(locationsArray);
+    //console.log(locationsArray);
     if (locationsArray.length < 1) {
         return;
     } else {
@@ -52,28 +60,79 @@ function getLocations () {
     }
 }
 
+//generate list of locations that match search
+
+
+function geocodeLocation () {
+    fetch(geocodingURL)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+
+    console.log(data);
+
+    console.log(data.length);
+
+    geocodedLocation = data[0].name + ',' + data[0].state + ',' + data[0].country;
+
+    if (data.length > 1) {
+        //put them in an array
+    }
+    else {
+        //select this result
+    }
+    
+
+    //let geoResult = 
+    //new array for possible geocode options
+    //populate an autopopulate.....? Or a dropdown.....?
+
+   /*let geocodeResultsArray;   
+    for (let i = 0; i < data.length; i++) {
+          //geocodeResultsArray.push(data.name)
+    }
+    console.log(geocodeResultsArray);*/
+    })
+    .catch(function (error) {
+        console.error(`This location has not been recognised, please try again.`, error);
+    });
+}
+
 
 //EVENT LISTENERS
-//search button
-//TODO - generate lat/long
+//search button - generate location list
 searchBtn.on('click', function() {
     event.preventDefault();
-    searchInput = $('#search-input').val();
-    console.log(`You searched ${searchInput}`); //TODO rm
+    searchInput = $('#search-input').val().trim();
+    //console.log(`You searched ${searchInput}`); //TODO rm
+    //TODO replace spaces with underscores or percentage 20 thing
+    generateLocations();
+    
+    //move these somewhere else
+    //geocodingURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + geocodedLocation + '&limit=10&appid=d1812ca69b57b9e8fd8ff23d673f0f07';
+    //geocodeLocation ();
+    c//onsole.log(`Geocoding URL: ${geocodingURL}`); //TODO rm
     
     //create button from this and add to aside
     addLocation (); 
 });
 
+//re-search button
+
+//separate 
+
 //TODO
+//error checking for Cities??
 
 //DONE
+//searched cities go in left column and creates a button
 
 //NOTES
 //query url needs latitude and longitude. city name will be entered on the web page. use api to translate city to lat/lond and put in variables for this url
-//searched cities go in left column and creates a button
 //when these buttons are clicked the weather info goes in the right
 //store it all in local storage
+
 
 //TESTS / TEMP
 renderLocations ();
